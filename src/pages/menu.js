05dc9f769361096d2241d3345eaa29cba311d60d
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-06-30 21:36:02 +0200

    Update menu.js

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -12,10 +12,7 @@ function Menu() {
 			queries: {
 				Authorization:
 					'Bearer 00D2X000001esWS!ASAAQFt1nPdywlTPnqeFBF7WSQdKKX9nKdr_oyTpajKccG5EZ03jtJJu7BS2tv2oLWhuwrc2V9WOv3tuOFHyJ0cuq8PA4rn1'
-			},
-			'Access-Control-Allow-Origin': '*',
-			Accept: 'application/json',
-			'Content-Type': 'application/json'
+			}
 		});
 
 		console.log(result.data);
