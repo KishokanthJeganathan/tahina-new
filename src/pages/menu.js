5b4a169604770849f2943db241f0e2d819fe124c
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-07-01 21:26:07 +0200

    Create menu.js

---
+++ src/pages/menu.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const jsforce = require('jsforce');
+const conn = new jsforce.Connection({
+	// you can change loginUrl to connect to sandbox or prerelease env.
+	loginUrl: 'https://tahina-test2-dev-ed.my.salesforce.com/'
+});
+
+const username = 'jeganathankishokanth@gmail.com';
+const password = 'Jeganathan12!';
+conn.login(username, password, function(err, userInfo) {
+	if (err) {
+		return console.error(err);
+	}
+	// Now you can get the access token and instance URL information.
+	// Save them to establish connection next time.
+	console.log(conn.accessToken);
+	console.log(conn.instanceUrl);
+	// logged in user property
+	console.log('User ID: ' + userInfo.id);
+	console.log('Org ID: ' + userInfo.organizationId);
+	// ...
+});
+
+const Menu = () => {
+	return <div>hello from menu page</div>;
+};
+
+export default Menu;
