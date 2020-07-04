b755e8026a4f0e44389f2babedd4cf1372a9d2d5
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-07-04 23:32:28 +0200

    Update menu.js

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,17 +2,16 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 function Menu() {
-	useEffect(async () => {
-		const result = await axios({
-			method: 'get',
-			url: `https://tahina-test2-dev-ed.my.salesforce.com/services/data/v48.0/query?q=SELECT+Product2Id,+Product2.Name+from+Pricebookentry+WHERE+Pricebook2Id='01s2X000002X6PxQAK'`,
-			headers: {
-				Authorization:
-					'Bearer 00D2X000001esWS!ASAAQG1hpJDqk71EfwEsDjQEOIVpxLK6B86D8g3yhU5tlUWBtcHoZhpbCtu.kinCTNz03pmHu4xgE_WN05YihmiORJ8e01tZ'
-			}
-		});
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await axios('https://tahina-test.herokuapp.com/');
 
-		console.log(result.data);
+			// setQuote(result.data);
+
+			console.log(result.data);
+		};
+
+		fetchData();
 	}, []);
 
 	return <ul />;
