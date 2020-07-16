1e9b5fb7b791095ce527731786d7f4fae8e974ef
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-07-16 10:12:19 +0200

    Update InstagramCarousal.js

--- src/components/index-page/instagram-carousal/InstagramCarousal.js
+++ src/components/index-page/instagram-carousal/InstagramCarousal.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { Col, Row } from 'react-bootstrap';
 import styles from '../instagram-carousal/instagramCarousal.module.css';
-import Carousel from '@brainhubeu/react-carousel';
-import '@brainhubeu/react-carousel/lib/style.css';
 import Background from '../instaBackgroundImage/Background';
-import Img from 'gatsby-image';
 import BackgroundImage from 'gatsby-background-image';
 import { FormattedMessage } from 'gatsby-plugin-intl';
 import Slider from 'react-slick';
@@ -33,6 +30,16 @@ const query = graphql`
 
 export default function Instagram() {
 	const data = useStaticQuery(query);
+	const instapostsNotNull = [];
+
+	const instaPosts = () => {
+		data.allInstaNode.edges.forEach((post) => {
+			if (post.node.localFile !== null) {
+				return instapostsNotNull.push(post);
+			}
+		});
+	};
+
 	const settings = {
 		dots: true,
 		infinite: true,
@@ -86,10 +93,11 @@ export default function Instagram() {
 						</a>
 					</p>
 				</Col>
+				{instaPosts()}
 
 				<Col xs={12} className={styles.instagram}>
 					<Slider {...settings}>
-						{data.allInstaNode.edges.map((image) => (
+						{instapostsNotNull.map((image) => (
 							<div>
 								<a
 									href="https://www.instagram.com/tahinawilcza/"
@@ -106,64 +114,3 @@ export default function Instagram() {
 		</Col>
 	);
 }
-
-// export default function Instagram() {
-// 	const data = useStaticQuery(query);
-
-// 	return (
-// 		<Col xs={12} className={styles.holder} id="contact">
-// 			<Row>
-// 				<Col xs={12}>
-// 					<h2 className={styles.hashtag}>#tahinawilcza</h2>
-// 					<p className={styles.CTA}>
-// 						<a
-// 							href="https://www.instagram.com/tahinawilcza/"
-// 							target="_blank"
-// 							rel="noopener noreferrer"
-// 							className={styles.link}
-// 						>
-// 							{<FormattedMessage id="instagram" />}
-// 						</a>
-// 					</p>
-// 				</Col>
-
-// 				<Col xs={12}>
-// 					<Row className={styles.imageHolder}>
-// 						<Col xs={8} sm={6} md={4} lg={3} className={styles.image1}>
-// 							<a href="https://www.instagram.com/tahinawilcza/" target="_blank" rel="noopener noreferrer">
-// 								<BackgroundImage
-// 									fluid={data.allInstaNode.edges[0].node.localFile.childImageSharp.fluid}
-// 									style={{ height: '30vh' }}
-// 								/>
-// 							</a>
-// 						</Col>
-// 						<Col xs={8} sm={6} md={4} lg={3} className={styles.image2}>
-// 							<a href="https://www.instagram.com/tahinawilcza/" target="_blank" rel="noopener noreferrer">
-// 								<BackgroundImage
-// 									fluid={data.allInstaNode.edges[1].node.localFile.childImageSharp.fluid}
-// 									style={{ height: '30vh' }}
-// 								/>
-// 							</a>
-// 						</Col>
-// 						<Col xs={8} sm={6} md={4} lg={3} className={styles.image3}>
-// 							<a href="https://www.instagram.com/tahinawilcza/" target="_blank" rel="noopener noreferrer">
-// 								<BackgroundImage
-// 									fluid={data.allInstaNode.edges[2].node.localFile.childImageSharp.fluid}
-// 									style={{ height: '30vh' }}
-// 								/>
-// 							</a>
-// 						</Col>
-// 						<Col xs={8} sm={6} md={4} lg={3} className={styles.image4}>
-// 							<a href="https://www.instagram.com/tahinawilcza/" target="_blank" rel="noopener noreferrer">
-// 								<BackgroundImage
-// 									fluid={data.allInstaNode.edges[3].node.localFile.childImageSharp.fluid}
-// 									style={{ height: '30vh' }}
-// 								/>
-// 							</a>
-// 						</Col>
-// 					</Row>
-// 				</Col>
-// 			</Row>
-// 		</Col>
-// 	);
-// }
